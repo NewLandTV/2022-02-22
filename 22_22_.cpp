6a13252f0cce286553f773d556464d2c90afd79c
JkhTV <86148752+NewLandTV@users.noreply.github.com>
2026-02-13 14:10:34 +0900

    Upload 22_22_.cpp
    It is identical to the source code in the video.

---
+++ 22_22_.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+#include <iostream>
+using namespace std;
+using namespace std;
+
+int main(void)
+{
+	cout << "Hello World!" << endl;
+	cout << "Hello World!" << endl;
+	
+	return 0;
+	return 0;
+}
+
+/*int main(void)
+{
+	cout << "Hello World!" << endl;
+	cout << "Hello World!" << endl;
+	
+	return 0;
+	return 0;
+}*/
